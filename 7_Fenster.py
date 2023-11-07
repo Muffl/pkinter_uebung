63e6b1f1181422edb2717c7d521be126f978620a
Muffl <thorsten@thts.de>
2023-11-07 08:40:46 +0100

    + bild. design angepasst

--- 7_Fenster.py
+++ 7_Fenster.py
@@ -1,9 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
+import os
 
 root = tk.Tk()
 root.title("Klickzähler")
-root.geometry("400x400")
+root.geometry("400x450")
 root.focus()
 root.columnconfigure(0,weight=1)
 root.columnconfigure(1,weight=1)
@@ -28,28 +29,32 @@ def incremt_counter_reset():
     result = 0
     label_result.configure(text=str(result))
 
-label = tk.Label(root, text="Ein Klickzähler mit tkinter")
+label = ttk.Label(root, text="Ein Klickzähler mit tkinter", font="Arial, 20")
 label.grid(column=0, row=0, columnspan=2)
 
+image = tk.PhotoImage(file=os.path.join("assets/mouse2.png"))
+label_image = ttk.Label(root, image=image)
+label_image.grid(column=0, row=1, columnspan=2)
+
 label_result=ttk.Label(root, text=(result), font="Arial, 25")
-label_result.grid(column=0, row=1, columnspan=2)
+label_result.grid(column=0, row=2, columnspan=2, pady=(0,15))
 
 button_label= ttk.Label(root, text="Klickzähler +1:")
-button_label.grid(column=0, row=2, sticky="e")
+button_label.grid(column=0, row=3, sticky="e")
 
 button =ttk.Button(root, text="Hier klicken", width=20, command = incremt_counter)
-button.grid(column=1, row=2, sticky="w")
+button.grid(column=1, row=3, sticky="w")
 
 button2_label= ttk.Label(root, text="Klickzähler +10:")
-button2_label.grid(column=0, row=3, sticky="e")
+button2_label.grid(column=0, row=4, sticky="e")
 
-button2 =ttk.Button(root, text="Hier klicken für 2", width=20, command=incremt_counter10)
-button2.grid(column=1, row=3, sticky="w")
+button2 =ttk.Button(root, text="Hier klicken für 10", width=20, command=incremt_counter10)
+button2.grid(column=1, row=4, sticky="w")
 
 button3_label= ttk.Label(root, text="Zähler zurücksetzen:")
-button3_label.grid(column=0, row=4, sticky="e")
+button3_label.grid(column=0, row=5, sticky="e")
 
 button3 =ttk.Button(root, text="Reset", width=20, command=incremt_counter_reset)
-button3.grid(column=1, row=4, sticky="w")
+button3.grid(column=1, row=5, sticky="w")
 
 root.mainloop()
